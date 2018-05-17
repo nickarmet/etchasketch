ec74cb75e16205247416bf42fc79393491491b31
Nick Armet <nick.armet@gmail.com>
2018-05-17 18:00:08 -0500

    append divs to parent container to prep for grid

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+let container = document.getElementsByClassName("container")[0];
+
 let createDiv = () => {
 	var verticalDivs = [];
 	var horizontalDivs = [];
 	for(let i = 1; i <= 16; ++i) {
 		verticalDivs[i] = document.createElement("div");
+		container.appendChild(verticalDivs[i]);
 		horizontalDivs[i] = document.createElement("div");
 	}
+	console.log(container)
 }
 
 createDiv();
